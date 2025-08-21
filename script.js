1897094b725e32b542eb4b958da9deefd7a8f7bc
danimiranda03 <dmm260996@gmail.com>
2025-08-21 11:20:09 -0500

    feat:le agregue parte responsiva

--- script.js
+++ script.js
@@ -8,3 +8,13 @@ window.addEventListener('load', () => {
         }, 100);
     });
 });
+
+// script.js
+document.addEventListener('DOMContentLoaded', () => {
+    const toggle = document.querySelector('.menu-toggle');
+    const menu = document.querySelector('.menu');
+
+    toggle.addEventListener('click', () => {
+        menu.classList.toggle('active');
+    });
+});
